ee27d52cc4240cccf121ba0ca0943bba3e5f2866
yinzj <1527698149@qq.com>
2017-12-13 16:45:09 +0800

    gui

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## 使用
 
 `tiny.exe 源文件(夹)路径 目标文件夹路径`
+
+`go build -ldflags="-H windowsgui -linkmode internal`
+
+`rsrc -manifest test.manifest -o rsrc.syso`
--- main.go
+++ main.go
@@ -13,6 +13,9 @@ import (
 	"TinyPNG/scheduler"
 	"strings"
 	"path/filepath"
+	"github.com/lxn/walk"
+	. "github.com/lxn/walk/declarative"
+	"github.com/labstack/gommon/log"
 )
 
 const (
@@ -30,6 +33,7 @@ create config.json
 var (
 	auth      []string
 	authIndex = 0
+	outTE     *walk.TextEdit
 )
 
 type Config struct {
@@ -73,6 +77,7 @@ func compress(file, path string) error {
 	if err != nil {
 		return err
 	}
+	defer f.Close()
 	var imgByte []byte
 	imgByte, err = ioutil.ReadAll(f)
 	if err != nil {
@@ -128,19 +133,28 @@ func download(url, name, path string) error {
 	if err != nil {
 		return err
 	}
+	defer f.Close()
 	_, err = io.Copy(f, resp.Body)
 	if err != nil {
 		return err
 	}
-	fmt.Printf(printInfo(filepath.Base(name)))
+	printProgress(printInfo(filepath.Base(name), "done"))
 	return nil
 }
 
-func printInfo(name string) string {
+func printProgress(s string) {
+	if len(os.Args) == 3 {
+		fmt.Println(s)
+	} else if outTE != nil {
+		outTE.SetText(outTE.Text() + s + "\r\n")
+	}
+}
+
+func printInfo(name, status string) string {
 	if len(name) < 36 {
-		return name + " " + strings.Repeat("-", 36-len(name)) + " Done\n"
+		return name + " " + strings.Repeat("-", 36-len(name)) + " " + status
 	} else {
-		return name[:30] + "... --- Done\n"
+		return name[:30] + "... --- " + status
 	}
 }
 
@@ -160,6 +174,43 @@ func initConfig() error {
 	return nil
 }
 
+func doCompress(src, dst string) {
+	var e error
+	var srcInfo os.FileInfo
+	srcInfo, e = os.Stat(src)
+	var dstInfo os.FileInfo
+	dstInfo, e = os.Stat(dst)
+	PrintE(e)
+	if dstInfo.IsDir() {
+		printProgress("---Start---\r\n")
+		if srcInfo.IsDir() {
+			s := scheduler.NewScheduler()
+			s.Start()
+			filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
+				if ".png" == filepath.Ext(info.Name()) || ".jpg" == filepath.Ext(info.Name()) {
+					s.Add(func() {
+						e := compress(path, dst)
+						if e != nil {
+							printProgress(printInfo(filepath.Base(path), "failed"))
+						}
+					})
+				}
+				return nil
+			})
+			s.Wait()
+		} else {
+			compress(src, dst)
+		}
+		printProgress("\r\n----End----")
+		if len(os.Args) == 3 {
+			os.Exit(0)
+		}
+	}
+	if len(os.Args) == 3 {
+		os.Exit(1)
+	}
+}
+
 func main() {
 	e := initConfig()
 	if e != nil {
@@ -168,32 +219,133 @@ func main() {
 	if len(os.Args) == 3 {
 		src := os.Args[1]
 		dst := os.Args[2]
-		var srcInfo os.FileInfo
-		srcInfo, e = os.Stat(src)
-		var dstInfo os.FileInfo
-		dstInfo, e = os.Stat(dst)
-		PrintE(e)
-		if dstInfo.IsDir() {
-			fmt.Println("---Start---\n")
-			if srcInfo.IsDir() {
-				s := scheduler.NewScheduler()
-				s.Start()
-				filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
-					if ".png" == filepath.Ext(info.Name()) || ".jpg" == filepath.Ext(info.Name()) {
-						s.Add(func() {
-							compress(path, dst)
-						})
-					}
-					return nil
-				})
-				s.Wait()
-			} else {
-				compress(src, dst)
-			}
-			fmt.Print("\n----End----")
-			os.Exit(0)
-		}
-		os.Exit(1)
+		doCompress(src, dst)
+	} else {
+		gui()
 	}
-	os.Exit(2)
+}
+
+type MyMainWindow struct {
+	*walk.MainWindow
+}
+
+func (mw *MyMainWindow) openFolder(prevFilePath string) (string, error) {
+	diaolg := new(walk.FileDialog)
+	diaolg.FilePath = prevFilePath
+	//diaolg.Filter = "Image Files (*.emf;*.bmp;*.exif;*.gif;*.jpeg;*.jpg;*.png;*.tiff)|*.emf;*.bmp;*.exif;*.gif;*.jpeg;*.jpg;*.png;*.tiff"
+	diaolg.Title = "Select"
+	if ok, err := diaolg.ShowBrowseFolder(mw); err != nil {
+		return "", err
+	} else if !ok {
+		return "", nil
+	}
+	return diaolg.FilePath, nil
+}
+
+func (mw *MyMainWindow) aboutAction() {
+	walk.MsgBox(mw, "About", "TinyPNGHelper", walk.MsgBoxIconInformation)
+}
+
+func gui() {
+	var srcLE, dstLE *walk.LineEdit
+	var mw = new(MyMainWindow)
+	icon, _ := walk.NewIconFromFile("./img/icon.ico")
+	MainWindow{
+		AssignTo: &mw.MainWindow,
+		Icon:     icon,
+		Title:    "TinyPNGHelper",
+		MinSize:  Size{600, 400},
+		Layout:   VBox{},
+		OnDropFiles: func(strings []string) {
+			if len(strings) > 0 {
+				f := strings[0]
+				info, _ := os.Stat(f)
+				if !info.IsDir() {
+					if ".png" != filepath.Ext(f) && ".jpg" != filepath.Ext(f) {
+						return
+					}
+				}
+				if srcLE.Focused() {
+					srcLE.SetText(f)
+				} else if dstLE.Focused() {
+					dstLE.SetText(f)
+				}
+			}
+		},
+		Children: []Widget{
+			Composite{
+				Layout: HBox{},
+				Children: []Widget{
+					Label{Text: "    源文件: "},
+					LineEdit{AssignTo: &srcLE},
+					PushButton{
+						Text: "浏览",
+						OnClicked: func() {
+							s, e := mw.openFolder(srcLE.Text())
+							if e != nil {
+								log.Error(e.Error())
+								return
+							}
+							srcLE.SetText(s)
+						},
+					},
+				},
+			},
+			Composite{
+				Layout: HBox{},
+				Children: []Widget{
+					Label{Text: "目标文件: "},
+					LineEdit{AssignTo: &dstLE},
+					PushButton{
+						Text: "浏览",
+						OnClicked: func() {
+							s, e := mw.openFolder(dstLE.Text())
+							if e != nil {
+								log.Error(e.Error())
+								return
+							}
+							dstLE.SetText(s)
+						},
+					},
+				},
+			},
+			Composite{
+				Layout: HBox{},
+				Children: []Widget{
+					TextEdit{
+						AssignTo: &outTE,
+						ReadOnly: true,
+						MinSize:  Size{400, 200},
+					},
+				},
+			},
+			Composite{
+				Layout: HBox{},
+				Children: []Widget{
+					PushButton{
+						Text:    "操作",
+						MaxSize: Size{100, 50},
+						OnClicked: func() {
+							go doCompress(srcLE.Text(), dstLE.Text())
+						},
+					},
+					PushButton{
+						Text:    "清空日志",
+						MaxSize: Size{100, 50},
+						OnClicked: func() {
+							outTE.SetText("")
+						},
+					},
+					PushButton{
+						Text:    "关于",
+						MaxSize: Size{100, 50},
+						OnClicked: func() {
+							mw.aboutAction()
+						},
+					},
+				},
+			},
+		},
+	}.Run()
+	srcLE.SetFocus()
 }
